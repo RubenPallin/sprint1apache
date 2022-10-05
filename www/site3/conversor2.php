4e753b85b66e461bc43586088dae9793f7e37555
Ruben Pallin Gonzalez <rpalling@fpcoruna.afundacion.org>
2022-10-05 14:00:27 +0200

    Conversor2.php Modificado Correctamente

--- www/site3/conversor2.php
+++ www/site3/conversor2.php
@@ -6,9 +6,13 @@
 	<?php
   	   if (isset($_POST["funidad"])) {
 		if ($_POST["funidad"] == "pulgada") {
-		    $v_pulgada = $_POST["fcantidad"];
+		    $v_pulgadas = $_POST["fcantidad"];
 		    $v_metros = $v_pulgadas * 0.0254;
-		    echi $v_pulgadas."pulgada (s) = ".$v_metros." metro(s)";
+		    echo $v_pulgadas."pulgada (s) = ".$v_metros." metro(s)";
+		} elseif ($_POST["funidad"] == "yarda") {
+		    $v_yardas = $_POST["fcantidad"];
+		    $v_metros = $v_yardas * 0.9144;
+		    echo $v_yardas." yarda (yd) = ".$v_metros." metros(s)";
 		} else {
 		  echo "unidad no soportada";
 		}
@@ -22,6 +26,8 @@
 	   
 	  <input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 	  <label for="pulgada_input">Pulgada(s)</label><br>
+	  <input type="radio" id="yarda_input" name="funidad" value="yarda">
+	  <label for="yarda_input">Yarda(yd)</label><br>
 	  <input type="radio" id="otro_input"  name="funidad" value="otro">
 	  <label for="otro_input">Otro</label><br>
 
